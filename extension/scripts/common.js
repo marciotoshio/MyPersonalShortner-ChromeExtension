aa370aca59e12ea13129b2ce2063bd123279717c
Marcio Toshio Ide <contato@marciotoshio.com.br>
2011-04-13 11:14:37 -0300

    js refactoring

---
+++ extension/scripts/common.js
@@ -0,0 +1,31 @@
+var common = {
+	createXhrRequest: function (url, method, params, callback) {
+		var request = new XMLHttpRequest();
+		request.open(method, settings.serviceUrl, true);
+		if(method == 'POST')
+		{
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		}
+		request.onreadystatechange  = callback;
+		request.send(params);
+	},
+	
+	handleError: function(error) {
+		document.getElementById('errorTitle').innerText = error.Message;
+		if(error.Errors != null && error.Errors.length > 0)
+		{
+			var ul = document.getElementById('errorMessages');
+			common.createErrorMessages(ul, error.Errors);
+		}
+		document.getElementById('error').style.display = "block";
+	},
+	
+	createErrorMessages: function(ul, messages) {
+		ul.innerHtml = "";
+		for(var i = 0; i < messages.length; i++) {
+			var li = document.createElement('li');
+			li.innerText = messages[i];
+			ul.appendChild(li);
+		}
+	}
+}
---
+++ extension/scripts/shortner.js
@@ -0,0 +1,33 @@
+var settings = {
+	serviceUrl: "http://localhost:7981/Api/Shorten"
+}
+
+var shortner = {
+	init: function() {
+		shortner.getCurrentUrl();
+	},
+	
+	getCurrentUrl: function() {
+		chrome.tabs.getSelected(null, function(tab){
+			common.createXhrRequest(settings.serviceUrl, 'POST', 'url=' + tab.url, shortner.handleRequest);
+		});
+	},
+	
+	handleRequest: function() {
+		if (this.readyState == 4) {
+			var result = JSON.parse(this.responseText);
+			if(this.status == 200) {
+				shortner.handleSuccess(result.Url);
+			}
+			else {
+				common.handleError(result);
+			}
+		}
+	},
+	
+	handleSuccess: function(url) {
+		document.getElementById('shortnedUrl').value = url;
+		document.getElementById('error').style.display = "none";
+		document.getElementById('loading').style.display = "none";
+	}
+}
